99a81331590972b1f04d0b9049fcdbcaf812541a
beqir <kuci.beqir@yahoo.com>
2018-09-09 17:46:03 +0200

    First commit

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -23,6 +23,7 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
+    disableHostCheck : true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
